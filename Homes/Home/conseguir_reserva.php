2b661f01903fb916c2589148e4c638278adef9b2
Guerig Eri <guerig.eri@gmail.com>
2022-05-25 22:31:51 +0200

    HOLA REI

---
+++ Homes/Home/conseguir_reserva.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<?php
+    require_once '../../inclusiones/conexion.php';
+    require_once '../../acciones/conseguir_zonas.php';
+    
+    // Guardar datos de la url 
+    $telefono = isset($_GET['telefono']) ? mysqli_real_escape_string($db, $_GET['telefono']) : false;    
+    
+    $reservaRealizada = getReservaCliente($db, $telefono);
+    
+    while($reserva = mysqli_fetch_assoc($reservaRealizada)):
+       // var_dump($reserva['tlfcli']);
+       // var_dump($telefono);
+        if(
+                $reserva['tlfcli'] == $telefono):
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sterla's Paradise</title>
+        <link rel="stylesheet" href="css/estilo.css"/>
+        <link rel="stylesheet" href="css/button-style.css"/>
+	<link rel="shortcut icon" href="images/logo_principal.png" />
+    </head>
+    <body>
+        <div id="central">
+
+            <div><img src="images/logo_principal.png" alt="sterlaParadise" title="sterlaParadise"></div>
+	
+        </div>
+        <h1 class="h1-reserva">
+            <?php
+                    $reserva = getReservaCliente($db, $telefono);
+                    if(!empty($reserva)):
+                        while($cliente = mysqli_fetch_assoc($reserva)):
+                            if($cliente['tlfcli'] == $telefono):
+                ?>
+                                Estimad@ <?=$cliente['nomcli']?> <?=$cliente['apecli']?> tiene una reserva en:
+            <?php
+                            endif;
+                    endwhile;
+                endif;
+            ?>
+            
+        </h1>
+        <form method='GET' action="">
+            
+            <div class="comprobarReserva"> 
+                <div class="divReserva">
+                    <label for="restaurante" class="label-reserva">Restaurante:</label>
+                    <?php
+                        $reserva = getReservaCliente($db, $telefono);
+                        if(!empty($reserva)):
+                            while($cliente = mysqli_fetch_assoc($reserva)):
+                                if($cliente['tlfcli'] == $telefono):
+                    ?>    
+                                    <?php
+                                    if($cliente['codrest'] == 1):
+                                    ?>
+                                        <input type="text" name="zonas" id="zonas" class="reservaDatos" value="Hawai" disabled="disabled"/>
+                                    <?php
+                                    elseif ($cliente['codrest'] == 2):
+                                    ?>
+                                        <input type="text" name="zonas" id="zonas" class="reservaDatos" value="Cerdeña" disabled="disabled"/>
+                                    <?php
+                                    elseif ($cliente['codrest'] == 3):
+                                    ?>
+                                        <input type="text" name="zonas" id="zonas" class="reservaDatos" value="Marbella" disabled="disabled"/>
+                                    <?php
+                                    elseif ($cliente['codrest'] == 4):
+                                    ?>
+                                        <input type="text" name="zonas" id="zonas" class="reservaDatos" value="London" disabled="disabled"/>
+                                    <?php
+                                        endif;
+                                    ?>
+                    <?php
+                                endif;
+                            endwhile;
+                        endif;
+                    ?>
+                </div>
+                <div class="divReserva">                    
+                    <label for="zonas" class="label-reserva">Zona: </label>
+                    <?php
+                        $reserva = getReservaCliente($db, $telefono);
+                        if(!empty($reserva)):
+                            while($cliente = mysqli_fetch_assoc($reserva)):
+                                if($cliente['tlfcli'] == $telefono):
+                    ?>    
+                                    <?php
+                                    if($cliente['codzona'] == 1):
+                                    ?>
+                                        <input type="text" name="zonas" id="zonas" class="reservaDatos" value="Interior" disabled="disabled"/>
+                                    <?php
+                                    elseif ($cliente['codzona'] == 2):
+                                    ?>
+                                        <input type="text" name="zonas" id="zonas" class="reservaDatos" value="Exterior" disabled="disabled"/>
+                                    <?php
+                                        endif;
+                                    ?>
+                    <?php
+                                endif;
+                            endwhile;
+                        endif;
+                    ?>
+                </div>
+                <div class="divReserva">
+                    <label for="fecha" class="label-reserva">Fecha: </label>
+                    <?php
+                        $reserva = getReservaCliente($db, $telefono);
+                        if(!empty($reserva)):
+                            while($cliente = mysqli_fetch_assoc($reserva)):
+                                if($cliente['tlfcli'] == $telefono):
+                    ?>
+                                    <input  type="text" name="fecha" id="fecha" class="reservaDatos" value="<?=$cliente['fecres']?>" disabled="disabled"/>
+                    <?php
+                                endif;
+                            endwhile;
+                        endif;
+                    ?> 
+                </div>
+                <div class="divReserva">
+                    <label for="hora" class="label-reserva">Hora: </label> 
+                    <?php
+                        $reserva = getReservaCliente($db, $telefono);
+                        if(!empty($reserva)):
+                            while($cliente = mysqli_fetch_assoc($reserva)):
+                                if($cliente['tlfcli'] == $telefono):
+                    ?>        
+                                    <input type="text" name="hora" id="hora" class="reservaDatos" value="<?=$cliente['horares']?>" disabled="disabled"/>
+                    <?php
+                                endif;
+                            endwhile;
+                        endif;
+                    ?>  
+                </div>
+                <div class="divReserva">
+                    <label for="numpersonas" class="label-reserva">Número de personas: </label> 
+                <?php
+                    $reserva = getReservaCliente($db, $telefono);
+                    if(!empty($reserva)):
+                        while($cliente = mysqli_fetch_assoc($reserva)):
+                            if($cliente['tlfcli'] == $telefono):
+                ?>
+                                <input type="text" name="numpersonas" id="numpersonas" class="reservaDatos" value="<?=$cliente['numper']?>" disabled="disabled"/>
+                <?php
+                            endif;
+                        endwhile;
+                    endif;
+                ?>
+                </div>
+                    
+                    <div class="divReserva">
+                        <h2 class="h2-reserva">Datos de contacto:</h2>
+                        <label for="telefono" class="label-reserva">Teléfono: </label>
+                        <?php
+                            $reserva = getReservaCliente($db, $telefono);
+                            if(!empty($reserva)):
+                                while($cliente = mysqli_fetch_assoc($reserva)):
+                                    if($cliente['tlfcli'] == $telefono):
+                        ?>
+                                        <input type="text" name="telefono" id="telefono" class="reservaDatos" value="<?=$cliente['tlfcli']?>" disabled="disabled"/><br/>
+                        <?php
+                                    endif;
+                                endwhile;
+                            endif;
+                        ?>
+                    </div>
+                    <div class="divReserva">
+                        <label for="email" class="label-reserva">Email: </label>
+                        <?php
+                            $reserva = getReservaCliente($db, $telefono);
+                            if(!empty($reserva)):
+                                while($cliente = mysqli_fetch_assoc($reserva)):
+                                    if($cliente['tlfcli'] == $telefono):
+                        ?>
+                                        <input type="text" name="email" id="email" class="reservaDatos" value="<?=$cliente['emailcli']?>" disabled="disabled"/><br/>
+                        <?php
+                                    endif;
+                                endwhile;
+                            endif;
+                        ?>
+                    </div>
+                    
+                <div class="comprobarReserva">
+                        <a href="index.html" class="a-reserva" id="boton-inicio">Volver al inicio</a>
+                </div>
+        </form>
+    </body>
+</html>
+<?php
+        //elseif($reserva['tlfcli'] != $telefono): header("Location:Index.html");
+        else: header("Refresh: 0; URL=Index.html");
+        endif;
+    endwhile;
+    
+?>
+
---
+++ acciones/guardar_reserva.php
@@ -0,0 +1,88 @@
+<?php
+
+if(!empty($_GET)){
+    require_once '../inclusiones/conexion.php';
+
+    
+    /* LECCIÓN DE HOY
+        Si no estableces el tipo se guarda como boolean, no nos habia pasado porque al hacer la validación ya colocabamos que el envio tenía que ser de tipo string
+    */
+    
+    // ---------------- Escapar datos ---------------
+    
+    // Reserva
+    $zona = isset($_GET['zonas']) ? mysqli_real_escape_string($db, $_GET['zonas']) : false;
+    $fecha = isset($_GET['fecha']) ? mysqli_real_escape_string($db, $_GET['fecha']) : false;
+    $hora = isset($_GET['hora']) ? mysqli_real_escape_string($db, $_GET['hora']) : false;
+    //$minuto = isset($_GET['3A']) ? mysqli_real_escape_string($db, $_GET['3A']) : false;
+    $numpersonas = isset($_GET['numpersonas']) ? mysqli_real_escape_string($db, $_GET['numpersonas']) : false;
+    $restaurante = isset($_GET['restaurante']) ? mysqli_real_escape_string($db, $_GET['restaurante']) : false;
+   
+    // Cliente
+    $nombre = isset($_GET['nombre']) ? mysqli_real_escape_string($db, $_GET['nombre']) : false;
+    $apellidos = isset($_GET['apellidos']) ? mysqli_real_escape_string($db, $_GET['apellidos']) : false;
+    $telefono = isset($_GET['telefono']) ? mysqli_real_escape_string($db, $_GET['telefono']) : false;
+    $email = isset($_GET['email']) ? mysqli_real_escape_string($db, $_GET['email']) : false;
+    
+    // Añadir datos en la base de datos
+    $sql = "SELECT realizarReserva('$fecha', '$hora', '$numpersonas', '$nombre', '$apellidos', '$telefono', '$email', '$zona', '$restaurante');";
+
+    $guardarDatos = mysqli_query($db, $sql);
+    
+    $restauranteAux = $restaurante;
+    
+    
+    function getReserva($db){
+        $sql = "SELECT * FROM reservas ORDER BY codreserva DESC LIMIT 1";
+        $reservaCliente = mysqli_query($db, $sql);
+
+        $resultado = array();
+
+        if($reservaCliente && mysqli_num_rows($reservaCliente) >= 1){
+            $resultado = $reservaCliente;
+        }
+
+        return $resultado;
+    }
+    
+    $reservaCliente = getReserva($db);
+    
+    while($reserva = mysqli_fetch_assoc($reservaCliente)){
+        if(
+               $reserva['apecli'] == $apellidos &&
+               $reserva['tlfcli'] == $telefono &&
+               $reserva['emailcli'] == $email
+                 
+        ){
+            require_once 'reservaRealizada.html';
+            echo "<h1 class='reservaGuardada comprobarReserva'>Su reserva se ha realizado correctamente Sr/Sra " . $reserva['nomcli'] . " " . $reserva['apecli'] . "</h1><br/>";
+            
+            
+            header("Refresh: 5; URL= ../Homes/Home/index.html");
+            
+        }else{
+            require_once 'reservaDenegada.html';
+            echo "<h1 class='reservaGuardada comprobarReserva'>No se puede realizar esta reserva.</h1>";
+            header("Refresh: 5; URL= ../Homes/Home/index.html");
+        }
+    }
+}
+
+  
+   // var_dump($comprobar);
+   //var_dump(mysqli_real_escape_string($db, $sql));
+   // var_dump(mysqli_fetch_assoc($comprobar));
+    
+    
+    // Mostrar la última reserva añadida
+    /*$comprobacion = "SELECT * FROM reservas ORDER BY codreserva DESC LIMIT 1";
+    $resultadoComprob = mysqli_query($db, $comprobacion);
+    
+    while($prueba = mysqli_fetch_assoc($resultadoComprob)){
+        var_dump($prueba);*/
+    
+    // Recargar la página principal después de realizar la reserva
+    
+    //header("Refresh: 5; URL=/sterlas_paradise/html/index.php");
+
+?>
